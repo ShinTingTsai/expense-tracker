8ad7f64239f84ba214f81fe6680aea6165ab2388
STTSAI <shinting.tsai@gmail.com>
2020-05-19 01:13:25 +0800

    feat: add record schema

--- Models/record.js
+++ Models/record.js
@@ -3,14 +3,10 @@ const Schema = mongoose.Schema;
 const recordSchema = new Schema({
   name: { type: String },
   category: { type: String },
-  date: { type: String },
-  amount: { type: String },
+  date: { type: Date },
+  amount: { type: Number },
 });
-module.exports = mongoose.model("Todo", todoSchema);
+module.exports = mongoose.model("Record", recordSchema);
 
 
 
-// 支出名稱：name
-// 類別：category
-// 日期：date
-// 金額：amount
