ede5caed4ce4850d4b9933c1636b8de247c5de30
STTSAI <shinting.tsai@gmail.com>
2020-05-19 00:33:08 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,16 @@
+// use express
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+// setup req and res
+// render
+app.get('/', (req, res) => {
+ res.send("hello world");
+})
+
+// app.listen
+app.listen(port, () => {
+console.log(`Express is running on http://localhost:${port}`)
+})
