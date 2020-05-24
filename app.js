fc2a3675c5ef7ca4c2448ed209893120663ee1ed
STTSAI <shinting.tsai@gmail.com>
2020-05-24 22:42:29 +0800

    chore: heroku init

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const routes = require('./routes')
 require('./config/mongoose')
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT || 3000
 // setup template engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -22,6 +22,6 @@ handlebars.registerHelper('equal', function (item1, item2, options) {
   return (item1 === item2) ? options.fn(this) : options.inverse(this)
 })
 
-app.listen(port, () => {
-console.log(`Express is running on http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is running on http://localhost:${PORT}`)
 })
